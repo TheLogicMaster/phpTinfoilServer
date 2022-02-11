0dffcc3607cb7f1ce2f3b36b82dc569baccb1459
Justin Marentette <justinmarentette11@gmail.com>
2022-02-11 14:54:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<?php
+<?php 
 $gameDir = "Games/";
-$ignoreDirs = array('.', '..');
+$ignoreDirs = array('.', '..', '.convert');
 $games = array_diff(scandir($gameDir), $ignoreDirs);
 $addr = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/$gameDir";
 $output = '{"files":[';
-foreach ($games as $game) 
-  $output = $output. '{"url":"'. "$addr$game". '","size":'. filesize($gameDir. $game). '},';
+foreach ($games as $game) {
+  $dir = rawurlencode($game);
+  $output = $output. '{"url":"'. "$addr$dir". '","size":'. filesize($gameDir. $game). '},';
+}
 $output = rtrim($output, ","). '],"directories":[],"success":"Hello Piracy!"}';
 echo $output;
 ?>
